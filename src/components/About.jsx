7b92f161356282c914bfeedf984eb0bba231c4fa
GlenKurio <gleb.kurip@gmail.com>
2023-11-14 20:24:03 -0600

    more design

---
+++ src/components/About.jsx
@@ -0,0 +1,163 @@
+import Heading from "./atoms/Heading";
+import styled from "styled-components";
+import GridContainer from "./layout/GridContainer";
+import Paragraph from "./atoms/Paragraph";
+import Button from "./atoms/Button";
+import { devices } from "./layout/Queries";
+
+const StyledAboutContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 4rem;
+  margin: 4rem 1rem 1rem 1rem;
+  height: min-content;
+`;
+
+const StyledContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2rem;
+  align-items: start;
+`;
+
+const StyledTextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  color: var(--color-gray-900);
+  text-align: left;
+`;
+
+const ImageComposition = styled.div`
+  position: relative;
+
+  & img {
+    max-width: 60%;
+    max-height: 800px;
+    box-shadow: var(--shadow-sm);
+    border-radius: 9px;
+    position: absolute;
+
+    transition: all 0.2s;
+
+    outline-offset: 0.5rem;
+  }
+
+  & img:nth-child(1) {
+    left: 0;
+    top: -1.5rem;
+  }
+  & img:nth-child(2) {
+    right: 0;
+    top: 1.5rem;
+  }
+  & img:nth-child(3) {
+    left: 20%;
+    top: 6rem;
+  }
+
+  & img:hover {
+    outline: 0.85rem solid var(--color-brand-500);
+
+    transform: scale(1.05) translateY(-0.3rem);
+    box-shadow: var(--shadow-md);
+    z-index: 20;
+  }
+
+  & img:not(:hover) {
+    transform: scale(0.9);
+  }
+
+  @media ${devices.laptop} {
+    background-position: center;
+    min-height: 40vh;
+  }
+  @media ${devices.tablet} {
+    background-position: center;
+    min-height: 45vh;
+    & img:nth-child(1) {
+      left: 0;
+      top: -1.5rem;
+    }
+    & img:nth-child(2) {
+      right: 1rem;
+      top: 1.5rem;
+    }
+    & img:nth-child(3) {
+      left: 20%;
+      top: 6rem;
+    }
+  }
+  @media ${devices.mobileL} {
+    background-position: center;
+    min-height: 25vh;
+  }
+  @media ${devices.mobileM} {
+    background-position: center;
+    min-height: 20vh;
+  }
+  @media ${devices.mobileS} {
+    background-position: center;
+    min-height: 20vh;
+  }
+`;
+
+function About() {
+  return (
+    <StyledAboutContainer>
+      <Heading color="gradient" type="h4" as="h2">
+        Exciting tours for adventurous people
+      </Heading>
+      <GridContainer type="autofitMD">
+        <StyledContentContainer>
+          <StyledTextContainer>
+            <Heading type="h6" as="h3">
+              You`re going to fall in love with nature
+            </Heading>
+            <Paragraph>
+              From the moment you set foot on the African soil, you`ll be
+              immersed in a world where nature reigns supreme. Feel your heart
+              race as you witness a herd of majestic elephants wandering across
+              the vast savannah, or marvel at the graceful movements of a
+              leopard perched high in a tree. Prepare to fall in love with the
+              breathtaking landscapes and awe-inspiring wildlife that Africa has
+              to offer.
+            </Paragraph>
+          </StyledTextContainer>
+          <StyledTextContainer>
+            <Heading type="h6" as="h3">
+              Live adventures like you never have before
+            </Heading>
+            <Paragraph>
+              Whether you`re embarking on a thrilling game drive through the
+              African bush, navigating the winding channels of the Okavango
+              Delta in a traditional mokoro canoe, or trekking through dense
+              rainforests in search of rare primates, every moment will be
+              filled with exhilaration. Push your limits, challenge your senses,
+              and create memories that will stay with you forever.
+            </Paragraph>
+          </StyledTextContainer>
+          <Button type="primary" size="md">
+            Discover tours
+          </Button>
+        </StyledContentContainer>
+        <ImageComposition>
+          <img
+            src="../../public/composition/comp-elephant.webp"
+            alt="People in boat on the river and group of Elephants on the shore."
+          />
+          <img
+            src="../../public/composition/comp-helicopter.webp"
+            alt="Helicopter flying over the Victoria Fall in Zimbabwe"
+          />
+          <img
+            src="../../public/composition/comp-lion.webp"
+            alt="People on safri shuttle in savannah looking on a adult male lion"
+          />
+        </ImageComposition>
+      </GridContainer>
+    </StyledAboutContainer>
+  );
+}
+
+export default About;
---
+++ src/components/Features.jsx
@@ -0,0 +1,132 @@
+import styled from "styled-components";
+import { devices } from "./layout/Queries";
+import GridContainer from "./layout/GridContainer";
+import Paragraph from "./atoms/Paragraph";
+import Heading from "./atoms/Heading";
+
+const StyledFatures = styled.div`
+  display: flex;
+  place-content: center;
+  padding: 4rem 0;
+
+  height: 120vh;
+  background-image: linear-gradient(
+      to right bottom,
+      rgba(234, 153, 123, 0.2),
+      rgba(250, 193, 122, 0.3)
+    ),
+    url("../../public/bg/elephants-3.webp");
+  background-size: cover;
+  background-position: top;
+
+  clip-path: polygon(0 10%, 100% 0, 100% 90%, 0 100%);
+  -webkit-clip-path: polygon(0 10%, 100% 0, 100% 90%, 0 100%);
+  color: var(--color-brand-50);
+  user-select: none;
+  margin: 3rem 1rem 2rem 1rem;
+
+  @media ${devices.tablet} {
+    background-position: center;
+  }
+  @media ${devices.mobileL} {
+    clip-path: polygon(0 5%, 100% 0, 100% 95%, 0 100%);
+    -webkit-clip-path: polygon(0 5%, 100% 0, 100% 95%, 0 100%);
+  }
+`;
+
+const FeatureCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  padding: 1.5rem;
+  background-color: rgba(19, 111, 99, 0.7);
+  border-radius: var(--border-radius-md);
+
+  & img {
+    width: 48px;
+    height: 48px;
+  }
+  transition: 0.3s;
+  &:hover {
+    scale: 1;
+  }
+
+  &:not(:hover) {
+    scale: 0.9;
+  }
+`;
+
+function Features() {
+  return (
+    <StyledFatures>
+      <GridContainer type="autofitFeatures">
+        <FeatureCard>
+          <span>
+            <img
+              src="../../public/icons/explore_black_24dp.svg"
+              aria-hidden
+              alt=""
+            />
+          </span>
+          <Heading type="h6" as="h6">
+            Explore the world
+          </Heading>
+          <Paragraph type="psm">
+            Let your adventurous spirit roam free as you explore uncharted
+            territories and create unforgettable memories.
+          </Paragraph>
+        </FeatureCard>
+        <FeatureCard>
+          <span>
+            <img
+              src="../../public/icons/south_america_black_24dp.svg"
+              aria-hidden
+              alt=""
+            />
+          </span>
+          <Heading type="h6" as="h6">
+            Meet Nature
+          </Heading>
+          <Paragraph type="psm">
+            Encounter Africa`s majestic wildlife up close, igniting a deep
+            connection with nature`s awe-inspiring beauty.
+          </Paragraph>
+        </FeatureCard>
+        <FeatureCard>
+          <span>
+            <img
+              src="../../public/icons/person_pin_circle_black_24dp.svg"
+              aria-hidden
+              alt=""
+            />
+          </span>
+          <Heading type="h6" as="h6">
+            Find Your Way
+          </Heading>
+          <Paragraph type="psm">
+            Uncover the magic of diverse landscapes, immerse in rich cultures,
+            and expand your horizons with us.
+          </Paragraph>
+        </FeatureCard>
+        <FeatureCard>
+          <span>
+            <img
+              src="../../public/icons/favorite_border_black_24dp.svg"
+              aria-hidden
+              alt=""
+            />
+          </span>
+          <Heading type="h6" as="h6">
+            Live healthier life
+          </Heading>
+          <Paragraph type="psm">
+            Embrace a healthier lifestyle in Africa`s natural playground,
+            revitalizing mind, body, and soul.
+          </Paragraph>
+        </FeatureCard>
+      </GridContainer>
+    </StyledFatures>
+  );
+}
+
+export default Features;
