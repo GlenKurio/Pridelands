9231ea67b8147fa0ada81ac1446be7fda25428c3
GlenKurio <gleb.kurip@gmail.com>
2023-11-27 16:47:10 -0600

    checkout

--- src/services/stripe/stripe.js
+++ src/services/stripe/stripe.js
@@ -1,5 +1,3 @@
 import { loadStripe } from "@stripe/stripe-js";
 
-export const stripePromise = loadStripe(
-  "pk_test_51OHB7KKpjAmKtlywKIy1HzWWN1DCkjkJ369K8WuW1gDqD9o5SroxNzirSE84ZogO9b4gSE22QEXxlKjlwRH22IJy00Zg7XM3X0"
-);
+export const stripePromise = loadStripe(process.env.VITE_STRIPE_PUBLIC_KEY);
